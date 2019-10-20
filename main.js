d1e931b9f9d549dac854aabd23562ec138ff04f5
deezone <darrendouglas.lee@gmail.com>
2019-10-20 17:42:28 -0400

    feat: Functional files for demo of JustGage

---
+++ main.js
@@ -0,0 +1,16 @@
+require('justgage');
+// var JustGage = require('justgage');
+
+var gauge = new JustGage({
+    id: "gauge", // the id of the html element
+    value: 50,
+    min: 0,
+    max: 100,
+    decimals: 2,
+    gaugeWidthScale: 0.6
+});
+
+// update the value randomly
+setInterval(() => {
+    gauge.refresh(Math.random() * 100);
+}, 5000)
